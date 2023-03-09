0fe2732880308684316ec7a9f1f1abadaaad047f
teetom <45373861+teetom-code@users.noreply.github.com>
2023-03-09 16:14:10 +1100

    Create SG

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ data "aws_ami" "app_ami" {
 data "aws_vpc" "default" {
   default = true
 }
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
